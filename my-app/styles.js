5e405a09bb478e4f091230f3f2b4a55492fbaa03
johnkenjikono <johnk.kono@gmail.com>
2025-04-08 09:28:59 -0400

    Added React environment, created App.js code for home page and rough Home, About, Contact Us code.

---
+++ my-app/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  header: {
+    paddingTop: 50,
+    paddingBottom: 20,
+    backgroundColor: '#1e90ff',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  slogan: {
+    fontSize: 16,
+    color: '#f0f8ff',
+  },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
